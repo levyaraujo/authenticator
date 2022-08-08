b3f3957cdfe0024eda2a79b920ebfd7e8ba1efa9
Levy Araujo <lev0x79@gmail.com>
2022-08-08 19:38:37 -0300

    :card_file_box: Fix Usuario model id related issues

--- api/models.py
+++ api/models.py
@@ -1,19 +1,55 @@
 from typing import List
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class Usuario(AbstractUser):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=70)
+    username = None
     bio = models.TextField()
     phone = models.CharField(max_length=9)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='api/images')
     REQUIRED_FIELDS: List = []
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD: str = 'email'
+    objects = UserManager()
 
     def __str__(self):
         return self.name
