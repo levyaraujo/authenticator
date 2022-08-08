d7590fa2696740f07eee224a57010319099d6fd9
Levy Araujo <lev0x79@gmail.com>
2022-08-08 19:39:05 -0300

    :sparkles: Add api endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from typing import List
 
 from django.urls import path
 
-urlpatterns: List = [
+from .views import Login, Signup
 
+urlpatterns: List = [
+    path('signup/', Signup.as_view(), name='register'),
+    path('login/', Login.as_view(), name='login'),
 ]
