f5557d169b727e027b76ce1011db5930002047a3
Levy Araujo <lev0x79@gmail.com>
2022-08-09 18:49:15 -0300

    :sparkles: Add user/ endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,10 @@ from typing import List
 
 from django.urls import path
 
-from .views import Login, Signup
+from .views import Login, Signup, UserPage
 
 urlpatterns: List = [
     path('signup/', Signup.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
+    path('user/', UserPage.as_view(), name='user_page'),
 ]
